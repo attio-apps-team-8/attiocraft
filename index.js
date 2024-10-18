b4c8b6f6e5360907318d72d00e9afe7ed82a47fc
Dominic Eccleston <domeccleston@Dominics-MacBook-Air.local>
2024-10-18 13:05:14 +0200

    ignore

---
+++ index.js
@@ -0,0 +1,28 @@
+const flyingSquid = require('flying-squid')
+
+
+flyingSquid.createMCServer({
+  version: '1.16.4',  // You can specify the Minecraft version
+  port: 25565,        // Default Minecraft server port
+  'max-players': 10,  // Maximum players allowed
+  onlineMode: false,  // Set to true if you want players to authenticate with Mojang
+  motd: 'Attio Minecraft',  // Message of the day
+  logging: true,  // Enable server logging
+  gameMode: 0,    // 0: Survival, 1: Creative, 2: Adventure, 3: Spectator
+  generation: {
+    name: 'diamond_square',
+    options: {
+      worldHeight: 80
+    }
+  },
+  difficulty: 0,  // Difficulty level: 0 (Peaceful), 1 (Easy), 2 (Normal), 3 (Hard)
+  'view-distance': 10,  // View distance for players
+  'player-list-text': {
+    header: 'Welcome to Attio Minecraft.',
+    footer: 'Enjoy your stay!'
+  },
+  'everybody-op': false,  // Set to true if you want every player to be an operator
+  'max-entities': 100,
+  plugins: {
+  },
+})
