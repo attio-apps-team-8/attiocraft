07bdd102cb11fa520cc10c8325afb71fedbcef42
Braden Marshall <braden1996@hotmail.co.uk>
2024-10-18 16:58:27 +0200

    More commands

--- attio-api-plugin.js
+++ attio-api-plugin.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 var events = require('./events');
+const Vec3 = require('vec3').Vec3
 
 const ATTIO_API_KEY = '210268df87538f59811ad8065558a32b4dfbcc18e31dcdf4fa0efb83309018b8'
 
@@ -32,11 +33,46 @@ module.exports.player = function (player, serv) {
     }
   });
 
+  events.addListener('throw-player', async ({ username }) => {
+    if (username !== player.username) return
+    console.log('[SERVER] throw', username)
+    player.takeDamage({
+      sound: 'game.player.hurt',
+      damage: 0,
+      velocity: new Vec3(40, 60, 10),
+    })
+
+  })
+
   events.addListener('kill-player', async ({ username }) => {
+    if (username !== player.username) return
     console.log('[SERVER] kill-player', username)
-    if (username === player.username) {
-      player.updateHealth(0)
-    }
+    player.updateHealth(0)
+  })
+
+  events.addListener('kick-player', async ({ username }) => {
+    if (username !== player.username) return
+    console.log('[SERVER] kick-player', username)
+    player.kick()
+  })
+
+  events.addListener('give-player', async ({
+    username,
+    itemId,
+    number
+  }) => {
+    if (username !== player.username) return
+    player.handleCommand(`give ${username} ${itemId} ${number || 1}`);
+  })
+
+  events.addListener('broadcast-player', async ({
+    username,
+    message
+  }) => {
+    if (username !== player.username) return
+    player.chat(message);
+
+    res.status(200)
   })
 
   events.addListener('spawn-blocks', async ({
@@ -45,13 +81,14 @@ module.exports.player = function (player, serv) {
   }) => {
     if (username !== player.username) return
 
+    console.log(`[SERVER] spawn-blocks`)
+
     const position = {
       x: Math.round(player.position.x),
       y: Math.round(player.position.y),
       z: Math.round(player.position.z),
     }
 
-    console.log(`setblock ${position.x} ${position.y - 1} ${position.z} ${itemId}`)
 
     player.handleCommand(
       `setblock ${position.x} ${position.y - 1} ${position.z} ${itemId}`
--- index.js
+++ index.js
@@ -7,10 +7,58 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+app.get('/throw-player', (req, res) => {
+  const username = req.query.username
+  console.log('[REQUEST] throw-player', username)
+  events.emit('throw-player', { username })
+  res.status(200)
+  res.send(true)
+})
+
 app.get('/kill-player', (req, res) => {
   const username = req.query.username
   console.log('[REQUEST] kill-player', username)
   events.emit('kill-player', { username })
+  res.status(200)
+  res.send(true)
+})
+
+app.get('/kill-players', (req, res) => {
+  const usernames = req.query.usernames.split(',')
+  console.log('[REQUEST] kill-players', usernames)
+  for (const username of usernames) {
+    events.emit('kill-player', { username })
+  }
+  res.status(200)
+  res.send(true)
+})
+
+app.get('/kick-player', (req, res) => {
+  const username = req.query.username
+  console.log('[REQUEST] kick-player', username)
+  events.emit('kick-player', { username })
+  res.status(200)
+  res.send(true)
+})
+
+app.get('/give-player', (req, res) => {
+  const username = req.query.username
+  const itemId = req.query.itemId
+  const number = req.query.number
+  console.log('[REQUEST] give-player', username, itemId, number)
+  events.emit('give-player', { username, itemId, number })
+  res.status(200)
+  res.send(true)
+})
+
+app.get('/broadcast-players', (req, res) => {
+  const usernames = req.query.usernames.split(',')
+  const message = req.query.message
+  console.log('[REQUEST] broadcast-players', usernames, message)
+  for (const username of usernames) {
+    events.emit('broadcast-player', { username, message })
+  }
   res.status(200)
   res.send(true)
 })
@@ -28,7 +76,6 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
 flyingSquid.createMCServer({
   version: '1.16.5',  // You can specify the Minecraft version
   port: 25565,        // Default Minecraft server port
@@ -43,7 +90,7 @@ flyingSquid.createMCServer({
       worldHeight: 80
     }
   },
-  difficulty: 0,  // Difficulty level: 0 (Peaceful), 1 (Easy), 2 (Normal), 3 (Hard)
+  difficulty: 2,  // Difficulty level: 0 (Peaceful), 1 (Easy), 2 (Normal), 3 (Hard)
   'view-distance': 10,  // View distance for players
   'player-list-text': {
     header: 'Welcome to Attio Minecraft.',
